494a93c7bf676a9e0e4eecbf40fc2125061093fc
Siphalor <xyx0125@gmail.com>
2019-03-27 20:25:49 +0100

    Version 1.0.3
    Update to 19w13a

--- src/main/java/de/siphalor/mousewheelie/client/mixin/MixinContainerScreen.java
+++ src/main/java/de/siphalor/mousewheelie/client/mixin/MixinContainerScreen.java
@@ -9,12 +9,17 @@ import net.minecraft.container.Slot;
 import net.minecraft.container.SlotActionType;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.TextComponent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ContainerScreen.class)
 public abstract class MixinContainerScreen extends Screen implements IContainerScreen {
+	protected MixinContainerScreen(TextComponent textComponent_1) {
+		super(textComponent_1);
+	}
+
 	@Shadow protected abstract Slot getSlotAt(double double_1, double double_2);
 
 	@Shadow protected abstract void onMouseClick(Slot slot_1, int int_1, int int_2, SlotActionType slotActionType_1);
@@ -29,25 +34,25 @@ public abstract class MixinContainerScreen extends Screen implements IContainerS
 		boolean isPlayerSlot = hoveredSlot.inventory instanceof PlayerInventory;
 		boolean moveUp = scrollAmount * Core.scrollFactor < 0;
 		if((isPlayerSlot && moveUp) || (!isPlayerSlot && !moveUp)) {
-			if(isControlPressed()) {
+			if(hasControlDown()) {
 				for(Slot slot : container.slotList) {
 					if(slot.inventory == hoveredSlot.inventory) {
 						if(slot.getStack().isEqualIgnoreTags(hoveredStack))
 							onMouseClick(slot, slot.id, 0, SlotActionType.QUICK_MOVE);
 					}
 				}
-			} else if(isShiftPressed()) {
+			} else if(hasShiftDown()) {
 				onMouseClick(hoveredSlot, hoveredSlot.id, 0, SlotActionType.QUICK_MOVE);
 			} else {
 				mouseWheelie_sendSingleItem(hoveredSlot);
 			}
 		} else {
-			if(isShiftPressed() || isControlPressed()) {
+			if(hasShiftDown() || hasControlDown()) {
 				for(Slot slot : container.slotList) {
 					if(slot.inventory == hoveredSlot.inventory) continue;
 					if(slot.getStack().isEqualIgnoreTags(hoveredStack)) {
 						onMouseClick(slot, slot.id, 0, SlotActionType.QUICK_MOVE);
-						if(!isControlPressed())
+						if(!hasControlDown())
 							break;
 					}
 				}
