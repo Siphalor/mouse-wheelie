c304d6981f9af3858997e3ef04b5dd5eb3a5feb5
Siphalor <xyx0125@gmail.com>
2019-08-18 18:18:17 +0200

    Version 2.1.13master
    * Fixed the sort key binding

--- src/main/java/de/siphalor/mousewheelie/client/mixin/MixinAbstractContainerScreen.java
+++ src/main/java/de/siphalor/mousewheelie/client/mixin/MixinAbstractContainerScreen.java
@@ -24,7 +24,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @SuppressWarnings("WeakerAccess")
@@ -80,6 +79,8 @@ public abstract class MixinAbstractContainerScreen extends Screen implements ICo
 				}
 				return true;
 			});
+		} else if(ClientCore.SORT_KEY_BINDING.matchesKey(key, scanCode)) {
+			mouseWheelie_triggerSort();
 		}
 	}
 
@@ -124,12 +125,6 @@ public abstract class MixinAbstractContainerScreen extends Screen implements ICo
 				}
 			}
 		}
-	}
-
-	@Inject(method = "tick", at = @At("HEAD"))
-	public void tick(CallbackInfo callbackInfo) {
-		if(ClientCore.SORT_KEY_BINDING.wasPressed())
-			mouseWheelie_triggerSort();
 	}
 
 	@Override
