a6faf94260746b37140502247abc1be43a68125f
Siphalor <info@siphalor.de>
2020-12-27 14:11:04 +0100

    Fix incorrect waiters causing desyncs

--- src/main/java/de/siphalor/mousewheelie/client/MWClient.java
+++ src/main/java/de/siphalor/mousewheelie/client/MWClient.java
@@ -111,11 +111,11 @@ public class MWClient implements ClientModInitializer {
 		Hand hand = refillHand;
 		refillHand = null;
 		if (MWConfig.refill.offHand && hand.equals(Hand.OFF_HAND)) {
-			InteractionManager.push(new InteractionManager.PacketEvent(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND, BlockPos.ORIGIN, Direction.DOWN), triggerType -> triggerType == InteractionManager.TriggerType.CONTAINER_SLOT_UPDATE && MWClient.lastUpdatedSlot >= 36));
+			InteractionManager.push(new InteractionManager.PacketEvent(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND, BlockPos.ORIGIN, Direction.DOWN), triggerType -> triggerType == InteractionManager.TriggerType.CONTAINER_SLOT_UPDATE && MWClient.lastUpdatedSlot == 45));
 		}
 		SlotRefiller.refill();
 		if (MWConfig.refill.offHand && hand.equals(Hand.OFF_HAND)) {
-			InteractionManager.push(new InteractionManager.PacketEvent(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND, BlockPos.ORIGIN, Direction.DOWN), triggerType -> triggerType == InteractionManager.TriggerType.CONTAINER_SLOT_UPDATE && MWClient.lastUpdatedSlot >= 36));
+			InteractionManager.push(new InteractionManager.PacketEvent(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND, BlockPos.ORIGIN, Direction.DOWN), triggerType -> triggerType == InteractionManager.TriggerType.CONTAINER_SLOT_UPDATE && MWClient.lastUpdatedSlot == 45));
 		}
 
 		return true;
