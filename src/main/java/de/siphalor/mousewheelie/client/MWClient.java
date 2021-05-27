cc5b88fb139b7ee39a6a7b47eb94be048988475c
Siphalor <info@siphalor.de>
2021-05-27 12:23:42 +0200

    Fix #110 (emptying an offhand slot always triggers the refill offhand swapping)

--- src/main/java/de/siphalor/mousewheelie/client/MWClient.java
+++ src/main/java/de/siphalor/mousewheelie/client/MWClient.java
@@ -111,11 +111,25 @@ public class MWClient implements ClientModInitializer {
 		Hand hand = refillHand;
 		refillHand = null;
 		if (MWConfig.refill.offHand && hand.equals(Hand.OFF_HAND)) {
-			InteractionManager.push(new InteractionManager.PacketEvent(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND, BlockPos.ORIGIN, Direction.DOWN), triggerType -> triggerType == InteractionManager.TriggerType.CONTAINER_SLOT_UPDATE && MWClient.lastUpdatedSlot == 45));
+			// interaction only gets pushed softly so it can be removed if the refill was not successful
+			InteractionManager.interactionEventQueue.add(
+					new InteractionManager.PacketEvent(
+							new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND, BlockPos.ORIGIN, Direction.DOWN),
+							triggerType -> triggerType == InteractionManager.TriggerType.CONTAINER_SLOT_UPDATE && MWClient.lastUpdatedSlot == 45
+					)
+			);
 		}
-		SlotRefiller.refill();
-		if (MWConfig.refill.offHand && hand.equals(Hand.OFF_HAND)) {
-			InteractionManager.push(new InteractionManager.PacketEvent(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND, BlockPos.ORIGIN, Direction.DOWN), triggerType -> triggerType == InteractionManager.TriggerType.CONTAINER_SLOT_UPDATE && MWClient.lastUpdatedSlot == 45));
+		if (SlotRefiller.refill()) {
+			if (MWConfig.refill.offHand && hand.equals(Hand.OFF_HAND)) {
+				InteractionManager.push(
+						new InteractionManager.PacketEvent(
+								new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.SWAP_ITEM_WITH_OFFHAND, BlockPos.ORIGIN, Direction.DOWN),
+								triggerType -> triggerType == InteractionManager.TriggerType.CONTAINER_SLOT_UPDATE && MWClient.lastUpdatedSlot == 45
+						)
+				);
+			}
+		} else {
+			InteractionManager.clear();
 		}
 
 		return true;
