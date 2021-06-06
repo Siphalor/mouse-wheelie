fbcd2f9cb2f4d485a827e81483879d131a693722
Siphalor <info@siphalor.de>
2021-06-06 13:30:25 +0200

    Update to 1.17-rc1

--- src/main/java/de/siphalor/mousewheelie/client/mixin/gui/other/MixinAbstractButtonWidget.java
+++ src/main/java/de/siphalor/mousewheelie/client/mixin/gui/other/MixinAbstractButtonWidget.java
@@ -18,19 +18,19 @@
 package de.siphalor.mousewheelie.client.mixin.gui.other;
 
 import de.siphalor.mousewheelie.client.util.accessors.ISpecialClickableButtonWidget;
-import net.minecraft.client.gui.widget.AbstractButtonWidget;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(AbstractButtonWidget.class)
+@Mixin(ClickableWidget.class)
 public abstract class MixinAbstractButtonWidget {
 	@Shadow
 	protected abstract boolean clicked(double double_1, double double_2);
 
-	@Inject(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/AbstractButtonWidget;isValidClickButton(I)Z"), cancellable = true)
+	@Inject(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ClickableWidget;isValidClickButton(I)Z"), cancellable = true)
 	public void mouseClicked(double x, double y, int button, CallbackInfoReturnable<Boolean> callbackInfoReturnable) {
 		if (this.clicked(x, y)) {
 			if (this instanceof ISpecialClickableButtonWidget) {
