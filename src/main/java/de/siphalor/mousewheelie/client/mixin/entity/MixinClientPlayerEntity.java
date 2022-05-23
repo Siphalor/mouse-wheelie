a10fd6b5ea7c42c91dfb1b683c3187721114c842
Siphalor <info@siphalor.de>
2022-05-23 13:44:36 +0200

    Update to 1.19-pre1

--- src/main/java/de/siphalor/mousewheelie/client/mixin/entity/MixinClientPlayerEntity.java
+++ src/main/java/de/siphalor/mousewheelie/client/mixin/entity/MixinClientPlayerEntity.java
@@ -25,7 +25,9 @@ import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.network.encryption.PlayerPublicKey;
 import net.minecraft.util.Hand;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -34,8 +36,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerEntity.class)
 public abstract class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
-	public MixinClientPlayerEntity(ClientWorld clientWorld_1, GameProfile gameProfile_1) {
-		super(clientWorld_1, gameProfile_1);
+	public MixinClientPlayerEntity(ClientWorld world, GameProfile profile, @Nullable PlayerPublicKey publicKey) {
+		super(world, profile, publicKey);
 	}
 
 	@Inject(method = "closeScreen", at = @At("HEAD"))
